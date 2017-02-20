c74ea41c25c77c2d97961f73f87ac7ba9929e64c
Adhesion <adhesionmusic@gmail.com>
2017-02-20 18:21:40 -0500

    Draw stuff

---
+++ res/resources.res
@@ -0,0 +1,2 @@
+IMAGE bg "bg.png" 0
+SPRITE glasses "glasses1.png" 9 2 0
--- src/radintro.c
+++ src/radintro.c
@@ -1,10 +1,32 @@
 #include <genesis.h>
 
-int main() {
-	VDP_drawText("Hello World!!!", 10, 10);
-	VDP_drawText("RADMARS is DA BEST", 13, 15);
+#include "resources.h"
 
+Sprite* glassesSprite;
+
+void setupBackground() {
+	VDP_drawImage(PLAN_B, &bg, 0, 0);
+}
+
+void setupSprites() {
+	glassesSprite = SPR_addSprite(&glasses, 126, 106, TILE_ATTR(PAL1, TRUE, FALSE, FALSE));
+
+	// copy in palette from sprite data
+	VDP_setPalette(PAL1, glasses.palette->data);
+	
+	SPR_update();
+}
+
+int main() {
+	SPR_init(0, 0, 0);
+	// set all palette to black
+    VDP_setPaletteColors(0, (u16*)palette_black, 64);
+	
+	setupBackground();
+	setupSprites();
+	
 	while (1) {
+		SPR_update();
 		VDP_waitVSync();
 	}
 	
