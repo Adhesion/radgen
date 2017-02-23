93af11a356d28afd00fb4bf52ea4db21f205193c
Adhesion <adhesionmusic@gmail.com>
2017-02-22 22:47:41 -0500

    RADMARS

--- res/resources.res
+++ res/resources.res
@@ -1,4 +1,5 @@
 IMAGE bg "bg.png" 0
-SPRITE glasses "glasses1.png" 9 2 0
+SPRITE glasses "glasses.png" 9 2 0 5
+SPRITE radtext "radtext.png" 12 2 0 5
 XGM xgmmusic "radmars.vgm" 0
-WAV voice "radmars-voice-vocoded.wav" 0 16000
+WAV voice "radmars-voice-vocoded.wav" 5
--- src/radintro.c
+++ src/radintro.c
@@ -3,19 +3,46 @@
 #include "resources.h"
 
 #define RADMARS_VOICE_ID 64
+#define THAT_MOMENT 105
 
 Sprite* glassesSprite;
+Sprite* textSprite;
 u32 frameCounter = 0;
+s16 glassesPosX = 126;
+s16 glassesPosY = 1;
 
 void setupBackground() {
 	VDP_drawImage(PLAN_B, &bg, 0, 0);
 }
 
 void setupSprites() {
-	glassesSprite = SPR_addSprite(&glasses, 126, 106, TILE_ATTR(PAL1, TRUE, FALSE, FALSE));
+	glassesSprite = SPR_addSprite(&glasses, glassesPosX, glassesPosY, TILE_ATTR(PAL1, TRUE, FALSE, FALSE));
+	textSprite = SPR_addSprite(&radtext, 114, 150, TILE_ATTR(PAL2, TRUE, FALSE, FALSE));
 
 	// copy in palette from sprite data
 	VDP_setPalette(PAL1, glasses.palette->data);
+	VDP_setPalette(PAL2, radtext.palette->data);
+	
+	SPR_update();
+}
+
+void updateAnimation() {
+	if (frameCounter < THAT_MOMENT) {
+		glassesPosY++;
+		SPR_setPosition(glassesSprite, glassesPosX, glassesPosY);
+	}
+	else if (frameCounter == THAT_MOMENT) {
+		SPR_setAnimAndFrame(glassesSprite, 1, 2);
+	}
+	else if (frameCounter == THAT_MOMENT + 15) {
+		SPR_setAnim(glassesSprite, 0);
+	}
+	else if (frameCounter == THAT_MOMENT + 30) {
+		SPR_setAnim(textSprite, 1);
+	}
+	else if (frameCounter == THAT_MOMENT + 65) {
+		SPR_setAnim(textSprite, 2);
+	}
 	
 	SPR_update();
 }
@@ -29,13 +56,13 @@ int main() {
 	setupSprites();
 
 	XGM_startPlay(xgmmusic);
-	XGM_setPCM(RADMARS_VOICE_ID, voice, 42752);
+	XGM_setPCM(RADMARS_VOICE_ID, voice, 37376);
 	XGM_setLoopNumber(0);
 
 	while (1) {
-		SPR_update();
-
-		if (frameCounter == 128) {
+		updateAnimation();
+		
+		if (frameCounter == THAT_MOMENT + 55) {
 			XGM_startPlayPCM(RADMARS_VOICE_ID, 0, 1);
 		}
 
